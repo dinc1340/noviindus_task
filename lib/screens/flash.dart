26f381f9e72c82fd6d5bde48c362e06ba9a2b406
dheeshan <dheeshanibrahim@gmail.com>
2024-06-05 13:56:23 +0530

    Project created

---
+++ lib/screens/flash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlashScreen extends StatefulWidget {
+  const FlashScreen({super.key});
+
+  @override
+  State<FlashScreen> createState() => _FlashScreenState();
+}
+
+class _FlashScreenState extends State<FlashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [],),);
+  }
+}
